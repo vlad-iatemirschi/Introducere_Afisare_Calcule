245ed3f665862c7f052b01d69a25fbdd3157fc9b
iatemirschi vladislav cl 10D <70971250+vlad-iatemirschi@users.noreply.github.com>
2020-10-06 17:56:02 +0300

    Update problema__7.py

--- problema__7.py
+++ problema__7.py
@@ -1,3 +1,3 @@
-v=int(input("dati varsta Anei"))
-print("greutatea perfecta a Anei este de",2*v+8,"kg")
-print("inaltimea perfecta a Anei este de",5*v+80,"cm")
+m=int(input("dati varsta Anei"))
+print("greutatea perfecta a Anei este de",2*m+8,"kg")
+print("inaltimea perfecta a Anei este de",5*m+80,"cm")
